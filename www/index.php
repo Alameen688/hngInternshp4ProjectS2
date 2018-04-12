f4d65bb733e6ea2d9433bafe2165c56ee3b479ee
Alameen688 <ogundiran12@gmail.com>
2018-04-12 10:23:33 +0100

    First Commit

---
+++ www/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html><head>
+<meta http-equiv="content-type" content="text/html">
+<title>Stage One</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link rel="stylesheet" type="text/css" href="css/main.css">
+<link rel="stylesheet" type="text/css"  href="css/style.css">
+
+</head>
+
+<body>
+
+<div class="container">
+      <h1 class="title fader">HNG INTERNSHIP 4.0</h1>
+      <h4 class="name fader">Ogundiran Al-Ameen</h4>
+
+      <div class="time-container">
+        <?php
+          date_default_timezone_set('Africa/Lagos');
+          echo date('h:i A', time());
+        ?>
+      </div>
+</div>
+
+
+
+
+
+</body></html>
